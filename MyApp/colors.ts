e16dca588c7eff5c2d3962fdbc09b66bfca1d17e
markpopovich9 <betmen070809@gmail.com>
2026-03-06 18:12:34 +0300

    Colors

---
+++ MyApp/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+    white: '#FFFFFF',
+    plum: '#543C52',
+    grey: '#E9E5EE',
+    fog: '#FAF8FF',
+    green: '#22C55E',
+    lightgrey: '#CDCED2',
+    darkwhite: '#E2E0E8'
+}
